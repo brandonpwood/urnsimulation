41112dabbd4bf15ce70ba16a23094145a97b5202
brandonpwood <bwood0918@bths.edu>
2017-11-07 23:32:49 -0500

    Added better graphing

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 def trial(k, pi, pmax, n, m):
     '''Run a polya's urn simulation with n time-steps, m trials, k balls back,
@@ -27,7 +28,7 @@ def simulate(kmax, pmax, n, m):
             data[p, k] = trial(k, p, pmax, n, m)
     return data
 
-def plot(data, pmax, kmax):
+def plot3d(data, pmax, kmax):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection = '3d')
     X = []
@@ -41,10 +42,29 @@ def plot(data, pmax, kmax):
             Y.append(i)
 
     Z = np.reshape(data, (pmax*kmax))
-    print(Z)
-    ax.scatter(X, Y, Z, c ='b', marker = 'o')
+
+    ax.scatter(X, Y, Z, c = Z, marker = 'o', cmap = cm.Reds)
+    ax.set_xlabel('P/Pmax')
+    ax.set_ylabel('K')
+    ax.set_zlabel('Average P')
+
+    plt.show()
+
+def plot2d(data, pmax, kmax):
+    X = []
+    Y = []
+    for i in range(kmax):
+        for j in range(pmax):
+            X.append(j)
+    for i in range(pmax):
+        for j in range(kmax):
+            Y.append(i)
+
+    Z = np.reshape(data, (pmax*kmax))
+
+    plt.scatter(X, Y, c = Z, marker = 'o', cmap = cm.Reds)
 
     plt.show()
 
 data = simulate(100, 100, 5, 5)
-plot(data, 100, 100)
+plot2d(data, 100, 100)
